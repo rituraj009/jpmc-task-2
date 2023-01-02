0393dc9bad2fbe1028c035e00984b44533d111c8
Rituraj Singh <102078863+rituraj009@users.noreply.github.com>
2023-01-02 20:05:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,6 @@ class App extends Component<{}, IState> {
         super(props);
 
         this.state = {
-            // data saves the server responds.
-            // We use this state to parse data down to the child element (Graph) as element property
             data: [],
             showGraph: false,
         };
@@ -67,11 +65,6 @@ class App extends Component<{}, IState> {
                 </header>
                 <div className="App-content">
                     <button className="btn btn-primary Stream-button"
-                        // when button is click, our react app tries to request
-                        // new data from the server.
-                        // As part of your task, update the getDataFromServer() function
-                        // to keep requesting the data every 100ms until the app is closed
-                        // or the server does not return anymore data.
                             onClick={() => {
                                 this.getDataFromServer()
                             }}>
